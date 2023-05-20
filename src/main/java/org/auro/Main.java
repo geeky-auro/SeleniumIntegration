bde5049664d6d8ecb12157196df04edd77e9dc92
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-05-20 17:51:07 +0530

    Selenium Introduction

--- src/main/java/org/auro/Main.java
+++ src/main/java/org/auro/Main.java
@@ -5,13 +5,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Hello world!");
-    System.setProperty("webdriver.chrome.driver","C:\\chrome_driver\\chromedriver.exe");
+        //        System.out.println("Hello world!");
+        System.setProperty("webdriver.chrome.driver","C:\\chrome_driver\\chromedriver.exe");
         WebDriver driver=new ChromeDriver();
-    driver.manage().window().maximize();
-    driver.get("https://www.google.com/");
-        System.out.println(driver.getTitle());
-//        driver.quit();
+        //        Hit URL and Open the Browser
+        driver.get("https://countmycrunch.netlify.app/");
+        //        driver.quit();
+        //        Check the title of the Page..!
+        String st=driver.getTitle();
+        System.out.println("Title of the Page:"+st);
+
+        /**
+         *
+         * Check whether on hitting you are been sent to the same page or have been redirected to anyother page
+         * */
+
+        System.out.print("URL of the Web Page"+driver.getCurrentUrl());
+
+
+        // After test Close the browser (Closes the Current Site / Hovered Page ;))
+        driver.close();
+        // Closes all associated windows
+        driver.quit();
 
 
     }
