9c015bf76c1700ca2ab4166c2c2a510287ecebc9
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-05-20 15:21:44 +0530

    Initial commit

---
+++ src/main/java/org/auro/Main.java
@@ -0,0 +1,19 @@
+package org.auro;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+    System.setProperty("webdriver.chrome.driver","C:\\chrome_driver\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("https://www.google.com/");
+        System.out.println(driver.getTitle());
+//        driver.quit();
+
+
+    }
+
+}
