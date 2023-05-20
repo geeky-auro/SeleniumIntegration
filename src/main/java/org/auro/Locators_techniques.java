ead12ddfb338d62ca8e1030aa6a6f038dfdef346
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-05-20 23:50:28 +0530

    Importance of Locators in Selenium Web Driver

--- src/main/java/org/auro/Locators_techniques.java
+++ src/main/java/org/auro/Locators_techniques.java
@@ -2,6 +2,20 @@ package org.auro;
 
 public class Locators_techniques {
     public static void main(String[] args) {
-
+        System.out.println("Hello WOrld Welcome to Locators and Techniques ;)");
+        //Locators used by Selenium
+        /**
+         * ID
+         * Xpath
+         * CSS Selector
+         * name
+         * Class Name
+         * Tag Name
+         * Link Text
+         * Partial Link Text
+         *
+         *
+         *
+         * */
     }
 }
