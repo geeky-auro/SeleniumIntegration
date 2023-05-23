9af58f0c363098dbf649e2c7ee6487a674c30b6a
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2023-05-23 18:56:30 +0530

    Use of Assertion and Testng

---
+++ src/main/java/org/auro/Locators_techniques_2.java
@@ -0,0 +1,42 @@
+package org.auro;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class Locators_techniques_2 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","C:\\chrome_driver\\chromedriver.exe");
+        WebDriver driver =new ChromeDriver();
+        // We use Implicitwait becoz we need something to show while an even has occoured such as button click
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+
+        // <input type="text" placeholder="Username" id="inputUsername" value="">
+
+        driver.findElement(By.id("inputUsername")).sendKeys("rahulshetty");
+
+        // <input type="password" placeholder="Password" name="inputPassword" value="">
+        // Here we have name as the locator ;)
+
+        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.className("signInBtn")).click();
+        Thread.sleep(3500);
+        System.out.println(driver.findElement(By.tagName("p")).getText());
+        // SCan whether the Text matches or not without looking into Console if no error  then test passed successfully
+        Assert.assertEquals(driver.findElement(By.tagName("p")).getText(),"You are successfully logged in.");
+
+
+
+
+
+
+
+
+
+
+    }
+}
